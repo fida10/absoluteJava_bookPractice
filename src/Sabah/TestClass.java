1c4e0f555d031041707e02c46b2c92b168d9b3df
Sabah Ummie <sabah.ummie@gmail.com>
2022-08-10 19:03:02 -0400

    new lines

--- src/Sabah/TestClass.java
+++ src/Sabah/TestClass.java
@@ -2,6 +2,9 @@ package src.Sabah;
 
 public class TestClass {
 	public static void main(String[] args) {
+
 		System.out.println("Hello world");
+		System.out.println("Hello world 2");
+		System.out.println("Hello world 3");
 	}
 }
